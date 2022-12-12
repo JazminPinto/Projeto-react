962c9e930ecbb35b56953500b72d50d0e9ced935
jazmin pinto <jazminpinto61@gmail.com>
2022-12-12 12:38:07 -0300

    coloquei o cadastro dos produtos

--- src/Components/Cadastro/Cadastrar.js
+++ src/Components/Cadastro/Cadastrar.js
@@ -9,6 +9,10 @@ export default function Criar() {
   const [email, setEmail] = useState("");
   const [empreendimento, setEmpreendimento] = useState("");
   const [empreendedor, setEmpreendedor] = useState("");
+  const [nomeProduto, setNomeProduto] = useState("");
+  const [descricao, setDescricao] = useState("");
+  const [preco, setPreco] = useState("");
+  const [categoria, setCategoria] = useState("");
 
   const novoCadastro = async () => {
     const cadastroPost = {
@@ -17,6 +21,10 @@ export default function Criar() {
       email,
       empreendedor,
       empreendimento,
+      nomeProduto,
+      descricao,
+      preco,
+      categoria,
     };
 
     //crear cadastro
@@ -30,6 +38,7 @@ export default function Criar() {
   return (
     <div>
       <Cabecalho />
+      {/*cadastrar cliente*/}
       <div>
         <form>
           <span>Você é empreendedor?</span>
@@ -85,14 +94,83 @@ export default function Criar() {
             </label>
           </div>
           <div>
-            <span for='story'>Descreva seu empreendimento: </span> <br/>
-              <textarea id='story' name='story' 
-                rows='5' cols='33'
-                placeholder="Descreva seu empreendimento detalhadamente"
-                value={empreendimento}
-                onChange={(e) => setEmpreendimento(e.target.value)}
-              >
-            </textarea>
+            <span for="empreendimento">Descreva seu empreendimento: </span>{" "}
+            <br />
+            <textarea
+              id="empreendimento"
+              name="empreendimento"
+              rows="5"
+              cols="33"
+              placeholder="Descreva seu empreendimento detalhadamente"
+              value={empreendimento}
+              onChange={(e) => setEmpreendimento(e.target.value)}
+            ></textarea>
+          </div>
+
+          {/*cadastrar produto*/}
+          <div>
+            <span>Nome do produto: </span>
+            <label>
+              <input
+                type="text"
+                placeholder="produto do projeto X"
+                value={nomeProduto}
+                onChange={(e) => setNomeProduto(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <span>Descrição: </span>
+            <label>
+              <input
+                type="text"
+                placeholder="descriva seu produto brevemente"
+                value={descricao}
+                onChange={(e) => setDescricao(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <span>Preço: </span>
+            <label>
+              <input
+                type="text"
+                placeholder="R$ 100,00"
+                value={preco}
+                onChange={(e) => setPreco(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <span>Categoria: </span>
+            <label>
+              <input
+                type="text"
+                placeholder="serviços"
+                value={categoria}
+                onChange={(e) => setCategoria(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              <span> Emprendedor: </span>
+              <input
+                placeholder="Nome Sobrenome"
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              <span>Contato:</span>
+              <input
+                placeholder="(xx) xxxxx-xxxx"
+                value={telefone}
+                onChange={(e) => setTelefone(e.target.value)}
+              />
+            </label>
           </div>
         </form>
         <Link to="/">
